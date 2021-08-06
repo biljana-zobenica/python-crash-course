85a13af836c96f7a07ce13ee07ac53dab4ec4cc6
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-06 12:19:18 +0200

    hello-world file

---
+++ 02_Chapter/hello_world.py
@@ -0,0 +1,9 @@
+# print string
+print('Hello Python world!')
+
+# print variable value
+message = 'Hello Python world!'
+print(message)
+
+message = 'Hello Python Crash Course world!'
+print(message)
