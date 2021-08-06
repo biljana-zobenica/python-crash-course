de5cbb59a0b3b53548fda54b90ec9d560312e314
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-06 15:36:49 +0200

    Create personal message and change name cases

---
+++ 02_Chapter/name_cases.py
@@ -0,0 +1,4 @@
+person_name = 'Biljana'
+print(person_name.lower())
+print(person_name.upper())
+print(person_name.title())
---
+++ 02_Chapter/personal_message.py
@@ -0,0 +1,2 @@
+person_name = 'Biljana'
+print(f'Hello {person_name}, would you like to learn some Python today?')
