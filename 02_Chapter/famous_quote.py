8a8415ebc8e25bb226172b82f022d9c4e02b4d07
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-06 15:48:49 +0200

    Famous quote with variable and message definition

--- 02_Chapter/famous_quote.py
+++ 02_Chapter/famous_quote.py
@@ -1,1 +1,5 @@
 print('Mohanji once said, "If you connect to yourself, you have found everything."')
+
+famous_person = 'Mohanji'
+message = f'{famous_person} once said, "If you connect to yourself, you have found everything."'
+print(message)
