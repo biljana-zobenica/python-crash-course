026fa926b810b02e379890e9b50899bc66134565
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-08 19:26:14 +0200

    Print mode_of_transportation statements

---
+++ 03_Chapter/your_own_list.py
@@ -0,0 +1,6 @@
+mode_of_transportation = ['skoda', 'e-car', 'golf', 'peugeot', 'bicycle']
+print(f'I would like to own an electric version of {mode_of_transportation[0]} car.')
+print(f'So, I would be super happy to drive an {mode_of_transportation[1]}.')
+print(f'I believe that a {mode_of_transportation[2]} is a very reliable vehicle.')
+print(f'I really like a design of a new model of {mode_of_transportation[3]}.')
+print(f'The mode of transportation which I find most enjoyable, effordable and eco-friendly is a {mode_of_transportation[4]}.')
