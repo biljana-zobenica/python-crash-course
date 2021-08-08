035f712f2e20b46d9fbab572bacce09c2dde2220
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-08 21:10:06 +0200

    Change a guest list and dinner invitations

--- 03_Chapter/guest_list.py
+++ 03_Chapter/guest_list.py
@@ -1,5 +1,14 @@
-guest_list = ['Celine Dion', 'Novak Djokovic', 'Tina Turner']
+guest_list = ['Celine Dion', 'Novak Djokovic', 'Tina Turner', 'Gisele Bundchen']
 
 print(f'Hi {guest_list[0]}, I would like you to come to my celebration dinner.')
 print(f'Hi {guest_list[1]}, I would like you to come to my celebration dinner.')
 print(f'Hi {guest_list[2]}, I would like you to come to my celebration dinner.')
+print(f'Hi {guest_list[3]}, I would like you to come to my celebration dinner.')
+
+print(f'\n{guest_list[3]} would not make it to the celebration dinner.\n')
+guest_list[3] = 'Sandra Bullock'
+
+print(f'Hi {guest_list[0]}, I would like you to come to my celebration dinner.')
+print(f'Hi {guest_list[1]}, I would like you to come to my celebration dinner.')
+print(f'Hi {guest_list[2]}, I would like you to come to my celebration dinner.')
+print(f'Hi {guest_list[3]}, I would like you to come to my celebration dinner.')
