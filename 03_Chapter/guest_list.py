8a0ca81be01b137d0e68087c57277e11f52b6056
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-08 21:19:08 +0200

    Invite more guests

--- 03_Chapter/guest_list.py
+++ 03_Chapter/guest_list.py
@@ -12,3 +12,22 @@ print(f'Hi {guest_list[0]}, I would like you to come to my celebration dinner.')
 print(f'Hi {guest_list[1]}, I would like you to come to my celebration dinner.')
 print(f'Hi {guest_list[2]}, I would like you to come to my celebration dinner.')
 print(f'Hi {guest_list[3]}, I would like you to come to my celebration dinner.')
+
+print(f'\nHi everyone, I have just found a bigger dinner table. So, more guests will come.')
+
+guest_list.insert(0, 'Ryan Gosling')
+guest_list.insert(2, 'Jay Shetty')
+guest_list.append('Eddie Murphy')
+
+print(f'\n This is a new list:')
+print(guest_list)
+
+print(f'\nHi {guest_list[0]}, I would like you to come to my celebration dinner.')
+print(f'Hi {guest_list[1]}, I would like you to come to my celebration dinner.')
+print(f'Hi {guest_list[2]}, I would like you to come to my celebration dinner.')
+print(f'Hi {guest_list[3]}, I would like you to come to my celebration dinner.')
+print(f'Hi {guest_list[4]}, I would like you to come to my celebration dinner.')
+print(f'Hi {guest_list[5]}, I would like you to come to my celebration dinner.')
+print(f'Hi {guest_list[6]}, I would like you to come to my celebration dinner.')
+
+
