9c058ee1270b3fe1b1a78ae9c421d1cc70767055
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-08 21:03:06 +0200

    Create a guest list and dinner invitations

---
+++ 03_Chapter/guest_list.py
@@ -0,0 +1,5 @@
+guest_list = ['Celine Dion', 'Novak Djokovic', 'Tina Turner']
+
+print(f'Hi {guest_list[0]}, I would like you to come to my celebration dinner.')
+print(f'Hi {guest_list[1]}, I would like you to come to my celebration dinner.')
+print(f'Hi {guest_list[2]}, I would like you to come to my celebration dinner.')
