c01ecb0abfaf6c145926effecf89be1821d10287
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-08 19:14:10 +0200

    Print message to each member of a list of names

---
+++ 03_Chapter/greetings.py
@@ -0,0 +1,6 @@
+names = ['Sara', 'Maki', 'Dule', 'Nata', 'Nina']
+print(f'Hi {names[0]}, I am happy to have you as a friend!')
+print(f'Hi {names[1]}, I am happy to have you as a friend!')
+print(f'Hi {names[2]}, I am happy to have you as a friend!')
+print(f'Hi {names[3]}, I am happy to have you as a friend!')
+print(f'Hi {names[4]}, I am happy to have you as a friend!')
