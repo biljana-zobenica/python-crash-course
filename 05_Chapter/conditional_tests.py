d9846757eb2d8f62b6fe4c39ccb574de71511f1f
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-10 18:01:05 +0200

    Try out more conditional tests

--- 05_Chapter/conditional_tests.py
+++ 05_Chapter/conditional_tests.py
@@ -16,3 +16,32 @@ print(fruit == 'raspberry')
 animal = 'dog'
 print("\nIs animal == 'cat'? I predict False.")
 print(animal == 'cat')
+
+# test for equality
+size = 'medium'
+print("\nIs size == 'medium'? I predict True.")
+print(size == 'medium')
+
+# test for inequality
+size = 'medium'
+print("\nIs size != 'medium'? I predict False.")
+print(size != 'medium')
+
+# test using the lower() method
+fruit = 'Raspberry'
+print("\nIs fruit == 'raspberry'? I predict True.")
+print(fruit.lower() == 'raspberry')
+
+# numerical tests + using AND and OR keywords
+size = 5
+print(size == 5)
+print(size == 7)
+print(size > 2 and size < 7)
+print(size > 3 or size == 5)
+print(size <= 7)
+
+# check whether an item IS or NOT IN a list
+list = ['sun', 'sky', 'moon', 'mars', 'earth']
+print(list)
+print('sun' in list)
+print('moon' not in list)
