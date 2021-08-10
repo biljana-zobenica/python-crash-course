74140154d059b084fcac44aad8c5b2b565eb1e0c
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-10 22:18:11 +0200

    Create ordinal numbers

---
+++ 05_Chapter/ordinal_numbers.py
@@ -0,0 +1,13 @@
+# create a list of numbers
+numbers = list(range(1, 10))
+
+# loop through the list and make ordinal numbers
+for number in numbers:
+    if number == 1:
+        print(f'{number}st')
+    elif number == 2:
+        print(f'{number}nd')
+    elif number == 3:
+        print(f'{number}rd')
+    else:
+        print(f'{number}th')
