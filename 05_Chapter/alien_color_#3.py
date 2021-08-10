ecb76b811e37f9ec5661c58e2795e1dcecb2f649
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-10 18:59:21 +0200

    Alien color #3

---
+++ 05_Chapter/alien_color_#3.py
@@ -0,0 +1,29 @@
+# passes the first condition
+alien_color = 'green'
+
+if alien_color == 'green':
+    print('You have just earned 5 points!')
+elif alien_color == 'yellow':
+    print('You have just earned 10 points!')
+else: 
+    print('You have just earned 15 points!')
+
+# passes the second condition
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print('You have just earned 5 points!')
+elif alien_color == 'yellow':
+    print('You have just earned 10 points!')
+else: 
+    print('You have just earned 15 points!')
+
+# passes the 'else' condition
+alien_color = 'red'
+
+if alien_color == 'green':
+    print('You have just earned 5 points!')
+elif alien_color == 'yellow':
+    print('You have just earned 10 points!')
+else: 
+    print('You have just earned 15 points!')
