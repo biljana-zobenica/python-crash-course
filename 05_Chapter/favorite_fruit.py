4d8967010ae79af3937fb410954c13b8897ceb7e
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-10 19:16:01 +0200

    Favorite fruits task

---
+++ 05_Chapter/favorite_fruit.py
@@ -0,0 +1,8 @@
+favorite_fruits = ['avocado', 'pomegranate', 'blueberry']
+
+if 'blueberry' in favorite_fruits:
+    print('You really like blueberries!')
+if 'strawberry' in favorite_fruits:
+    print('You really like strawberries!')
+if 'avocado' in favorite_fruits:
+    print('You really like avocado!')
