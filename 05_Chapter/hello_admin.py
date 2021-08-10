ef956782e5f6b905230352a5ff730b7ceb67830c
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-10 21:02:29 +0200

    Create a no-users message

--- 05_Chapter/hello_admin.py
+++ 05_Chapter/hello_admin.py
@@ -1,7 +1,23 @@
+# hello messages to the users
 usernames = ['biksa', 'admin', 'eddie', 'spark', 'charlie']
 
-for user in usernames:
-    if user == 'admin':
-        print(f'Hello admin, would you like to see a status report?')
-    else:    
-        print(f'Hello {user}, thank you for logging in again.')
+if usernames == []:
+    print(f'We need to find some users!')
+else:
+    for user in usernames:
+        if user == 'admin':
+            print(f'Hello admin, would you like to see a status report?')
+        else:    
+            print(f'Hello {user}, thank you for logging in again.')
+
+# check that the code works when the list is empty
+usernames = []
+
+if usernames:
+    print(f'We need to find some users!')
+else:
+    for user in usernames:
+        if user == 'admin':
+            print(f'Hello admin, would you like to see a status report?')
+        else:    
+            print(f'Hello {user}, thank you for logging in again.')
