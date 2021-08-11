91503a811811021d420f98a05a64e35a1635fa5e
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-11 13:32:41 +0200

    Favorite numbers task

---
+++ 06_Chapter/favorite_numbers.py
@@ -0,0 +1,13 @@
+favorite_numbers = {
+    'Nick':'5', 
+    'Bob':'3', 
+    'Mike':'9',
+    'Steve':'12',
+    'Spike':'33',
+    }
+
+print(f"This is Nick's favorite number: {favorite_numbers['Nick']}.")
+print(f"This is Bob's favorite number: {favorite_numbers['Bob']}.")
+print(f"This is Mike's favorite number: {favorite_numbers['Mike']}.")
+print(f"This is Steve's favorite number: {favorite_numbers['Steve']}.")
+print(f"This is Spike's favorite number: {favorite_numbers['Spike']}.")
