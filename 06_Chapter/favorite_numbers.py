05cbe81ce50fd1d9db092a5cc9a1887242998eaf
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-11 21:25:20 +0200

    Favorite multiple numbers task

--- 06_Chapter/favorite_numbers.py
+++ 06_Chapter/favorite_numbers.py
@@ -1,9 +1,10 @@
+# version 1
 favorite_numbers = {
-    'Nick':'5', 
-    'Bob':'3', 
-    'Mike':'9',
-    'Steve':'12',
-    'Spike':'33',
+    'Nick':5, 
+    'Bob':3, 
+    'Mike':9,
+    'Steve':12,
+    'Spike':33,
     }
 
 print(f"This is Nick's favorite number: {favorite_numbers['Nick']}.")
@@ -11,3 +12,17 @@ print(f"This is Bob's favorite number: {favorite_numbers['Bob']}.")
 print(f"This is Mike's favorite number: {favorite_numbers['Mike']}.")
 print(f"This is Steve's favorite number: {favorite_numbers['Steve']}.")
 print(f"This is Spike's favorite number: {favorite_numbers['Spike']}.")
+
+# version 2 -> multiple favorite numbers
+favorite_numbers = {
+    'Nick':[5, 7, 9] ,
+    'Bob':[3, 1] ,
+    'Mike':[9, 19, 119],
+    'Steve':[12, 21],
+    'Spike':[33, 22, 99],
+    }
+
+for person, numbers in favorite_numbers.items():
+    print(f"These are {person.title()}'s favorite numbers:")
+    for number in numbers:
+        print(f"{number}")
