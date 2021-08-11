b18055dfac5604e0225b78399103bd4a62905217
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-11 13:28:07 +0200

    Print person dictionary

---
+++ 06_Chapter/person.py
@@ -0,0 +1,8 @@
+person = {
+    'first_name':'Eddie', 
+    'last_name':'Jackson', 
+    'age':'29', 
+    'city':'Stockholm'
+    }
+
+print(f"{person['first_name']} {person['last_name']} is {person['age']} years old and live in the city of {person['city']}.")
