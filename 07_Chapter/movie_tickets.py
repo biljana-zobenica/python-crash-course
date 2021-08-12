b3d032bbc5f15af6712d7cd0e499116866acc9ed
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-12 19:49:16 +0200

    Movie tickets task

---
+++ 07_Chapter/movie_tickets.py
@@ -0,0 +1,14 @@
+prompt = 'Please enter how old are you? '
+prompt += ("\nEnter 'quit' when you are finished.")
+
+while True:
+    age = int(input(prompt))
+    if age == 'quit':
+        break
+    if age < 3:
+        price = 'free'
+    elif age < 12:
+        price = '$10'
+    elif age > 12:
+        price = '$15'
+    print(f'Your movie ticket is {price}.')
