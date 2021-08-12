45afe43cd4a90935db835b4273f9acfdd3038cef
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-12 12:45:41 +0200

    Multiples of ten task

---
+++ 07_Chapter/multiples_of_ten.py
@@ -0,0 +1,6 @@
+number = int(input("Please, enter your number and I will see whether it is a multiple of 10 or not: "))
+
+if number % 10 == 0:
+    print(f"{number} is a multiple of 10.")
+else:
+    print(f"{number} is not a multiple of 10.")
