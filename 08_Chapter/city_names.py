a87aee89227295d34b5dea8ebf2479fc5558707f
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-13 21:30:29 +0200

    City names function task

---
+++ 08_Chapter/city_names.py
@@ -0,0 +1,6 @@
+def city_country(city, country):
+    print(f"{city.title()}, {country.title()}")
+
+city_country('belgrade', 'serbia')
+city_country('moscow', 'russia')
+city_country('rome', 'italy')
