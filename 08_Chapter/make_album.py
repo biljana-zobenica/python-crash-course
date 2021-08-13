9f1c89aedb924811a5bd5188c77d95051b2e057b
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-13 21:42:38 +0200

    Album + songs function task

--- 08_Chapter/make_album.py
+++ 08_Chapter/make_album.py
@@ -1,7 +1,9 @@
-def make_album (artist_name, album_name):
+def make_album (artist_name, album_name, songs=None):
     """Return a dictionary of information about an album."""
     info = {'artist':artist_name, 'album':album_name}
+    if songs:
+        info['songs'] = songs.title()
     return info
 
-album = make_album('queen', 'made in heaven')
+album = make_album('queen', 'made in heaven', 'i was born to love you')
 print (album)
