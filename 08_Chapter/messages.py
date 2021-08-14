bb14ab4d91c3dc3f48c7cec6d849ef8eccf32ae0
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-14 13:09:21 +0200

    Messages function task

---
+++ 08_Chapter/messages.py
@@ -0,0 +1,9 @@
+
+def show_messages (messages):
+    print("Here are your messages:")
+    for message in messages:
+        print(f"\t{message}")
+
+messages = ['Be brave!', 'Make each day count.', 'Keep calm and just smile!']
+
+show_messages(messages)
