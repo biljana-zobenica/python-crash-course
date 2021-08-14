aa10ed210f97c06e75d8e270f9e401ad5bf64bcd
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-14 13:28:51 +0200

    Pass a copy of a list to a function task

---
+++ 08_Chapter/archived_messages.py
@@ -0,0 +1,19 @@
+
+def send_messages (messages, sent_messages):
+    print(f"Printed messages:")
+    while messages:
+        current_message = messages.pop()
+        print(f"\t{current_message}")
+        sent_messages.append(current_message)
+    
+    print(f"Sent messages:")
+    for message in sent_messages:
+        print(f"\t{message}")
+
+messages = ['Be brave!', 'Make each day count.', 'Keep calm and just smile!']
+sent_messages = []
+
+send_messages(messages[:], sent_messages)
+
+print(messages)
+print(sent_messages)
