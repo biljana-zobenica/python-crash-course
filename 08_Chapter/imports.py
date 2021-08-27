b90b4b227bbc1a3054e0bea1a250e30a477ec603
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-08-27 16:00:53 +0200

    Import techniques task

---
+++ 08_Chapter/imports.py
@@ -0,0 +1,16 @@
+# import techniques
+
+# import module_name
+import printing_functions
+
+# from module_name import function_name
+from printing_functions import print_models
+
+# from module_name import function_name as fn
+from printing_functions import print_models as pm
+
+# import module_name as mn
+import printing_functions as pf
+
+# from module_name import * (all)
+from printing_functions import *
