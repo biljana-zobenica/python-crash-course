173ec47a4ff642daa062408c5d8e4c39236f577f
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-09-02 19:21:11 +0200

    Three restaurants task

---
+++ 09_Chapter/three_restaurants.py
@@ -0,0 +1,10 @@
+from restaurant import Restaurant
+
+restaurant_1 = Restaurant('Vege-Vegan', 'vegan')
+restaurant_2 = Restaurant('Raw', 'raw food')
+
+restaurant_1.describe_restaurant()
+restaurant_1.open_restaurant()
+
+restaurant_2.describe_restaurant()
+restaurant_2.open_restaurant()
